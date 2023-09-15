77fcb0d996a8886fe7780cf6f15b78fe6837b4fd
shrooot <yashrajsingh977@gmail.com>
2023-09-15 21:25:11 +0530

    fix cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 // }))
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://twitter-clone-frontend-production-1d63.up.railway.app/');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', true);
