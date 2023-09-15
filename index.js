84839c14bc4d55dab161f852be44208e167eaa7d
shrooot <yashrajsingh977@gmail.com>
2023-09-15 21:17:42 +0530

    fix cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://twitter-clone-frontend-production-1d63.up.railway.app/",
     credentials: true
 }))
 
